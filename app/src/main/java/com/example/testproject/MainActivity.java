4ca6d4f60d440123db8d2d79dad3d963f7f2ed4f
siwon <111dltldnjs@naver.com>
2021-12-25 01:53:02 +0900

    on/off 변수(Firebase) 삭제 => SharedPreferences로 대체, '포그라운드 서비스 종료'기능 추가

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -27,30 +27,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     public static MainActivity mainActivity;
     public static testService testService;
+    Intent serviceIntent;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<User> arrayList;
     private FirebaseDatabase database;
-    private DatabaseReference databaseReference,databaseReferenceTime;
-    private int openS,humanS,flameS;
-    private int openonoff,humanonoff,flameonoff,temp_humionoff;
-    private int temp,humi;
+    private DatabaseReference databaseReference, databaseReferenceTime;
+    private int openS, humanS, flameS;
+    private int openonoff, humanonoff, flameonoff, temp_humionoff;
+    private int temp, humi;
     private User user;
-    private TextView opentext,temp_humitext,flametext,humantext;
+    private TextView opentext, temp_humitext, flametext, humantext;
     private TextView appname;
-    private int openC,humanC,flameC;
-    private Intent serviceIntent;
-    private int nf,start;
+    private int openC, humanC, flameC;
+    private int nf, start, Bsave;
     private ImageView image;
     private String cctvip;
-    private LinearLayout humanL,flameL,openL;
-    private SharedPreferences pref;
-    private SharedPreferences.Editor editor;
+    private LinearLayout humanL, flameL, openL;
+    private SharedPreferences pref, btnsave;
+    private SharedPreferences.Editor editor, btn_save_editor;
+
     //파이어베이스에서 읽은 변화에 맞게 텍스트와 레이아웃에 변화를 주고 서비스로 데이터를 넘겨주는 메소드
-    private int send_Service(int onoff, int S, TextView text, LinearLayout L, int C, Intent intent,int Sv){
-        if(onoff==1) {
+    private int send_Service(int onoff, int S, TextView text, LinearLayout L, int C, Intent intent, int Sv) {
+        if (onoff == 1) {
             if (S == 1) {
                 text.setText("감지 됨");
                 L.setBackgroundResource(R.drawable.detected);
@@ -66,13 +68,24 @@ public class MainActivity extends AppCompatActivity {
                 C = 1;
 
             }
-        }
-        else{
+        } else {
             text.setText("센서 꺼짐");
             L.setBackgroundResource(R.drawable.not_detected);
 
         }
         return C;
+    }
+
+    void startF(Intent intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            startForegroundService(intent);
+
+        } else {
+
+            startService(intent);
+        }
+
     }
 
 
@@ -83,24 +96,24 @@ public class MainActivity extends AppCompatActivity {
 
         testService = new testService();
 
-        mainActivity =this;
+        mainActivity = this;
 
         layoutManager = new LinearLayoutManager(this);
         arrayList = new ArrayList<>();//User 객체를 담을 어레이 리스트(어댑터쪽으로)
 
-        opentext=(TextView)findViewById(R.id.opentext);
-        humantext=(TextView)findViewById(R.id.humantext);
-        flametext=(TextView)findViewById(R.id.flametext);
-        temp_humitext=(TextView)findViewById(R.id.temp_humitext);
-        appname=(TextView)findViewById(R.id.appname);
-        openL=(LinearLayout) findViewById(R.id.openL);
-        humanL=(LinearLayout)findViewById(R.id.humanL);
-        flameL=(LinearLayout)findViewById(R.id.flameL);
+        opentext = (TextView) findViewById(R.id.opentext);
+        humantext = (TextView) findViewById(R.id.humantext);
+        flametext = (TextView) findViewById(R.id.flametext);
+        temp_humitext = (TextView) findViewById(R.id.temp_humitext);
+        appname = (TextView) findViewById(R.id.appname);
+        openL = (LinearLayout) findViewById(R.id.openL);
+        humanL = (LinearLayout) findViewById(R.id.humanL);
+        flameL = (LinearLayout) findViewById(R.id.flameL);
 
         pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);//특정 상황에만 포그라운드를 실행할 수 있게 쉐어드프리퍼런스 사용.
         editor = pref.edit();
-
-
+        btnsave = getSharedPreferences("btnsave", Activity.MODE_PRIVATE);//특정 상황에만 포그라운드를 실행할 수 있게 쉐어드프리퍼런스 사용.
+        btn_save_editor = btnsave.edit();
 
 
         database = FirebaseDatabase.getInstance();       //파이어베이스 데이터베이스 연동
@@ -115,13 +128,16 @@ public class MainActivity extends AppCompatActivity {
         btnon.setOnClickListener(new View.OnClickListener() {//Off버튼
             @Override
             public void onClick(View v) {
+
+                image.setImageResource(R.drawable.disconnect);
+                editor.putInt("start", 1);//쉐어드 프리퍼런스에 1값 저장
+                editor.apply();
+                btn_save_editor.putInt("Bsave", 0);
+                btn_save_editor.apply();
                 btnon.setVisibility(View.INVISIBLE);
                 btnoff.setVisibility(View.VISIBLE);
                 appname.setText("OFF");
-                image.setImageResource(R.drawable.disconnect);
-                databaseReference.child("User_01").child("nf").setValue(0);
-                editor.putInt("start", 1);//쉐어드 프리퍼런스에 1값 저장
-                editor.apply();
+                stopService(serviceIntent);
 
             }
         });
@@ -129,14 +145,18 @@ public class MainActivity extends AppCompatActivity {
         btnoff.setOnClickListener(new View.OnClickListener() {//on버튼
             @Override
             public void onClick(View v) {
+
+                editor.putInt("start", 1);//쉐어드 프리퍼런스
+                editor.apply();
+                btn_save_editor.putInt("Bsave", 1);
+                btn_save_editor.apply();
                 btnon.setVisibility(View.VISIBLE);
                 btnoff.setVisibility(View.INVISIBLE);
                 image = (ImageView) findViewById(R.id.connect);
                 Glide.with(getApplicationContext()).load(R.drawable.connect2).into(image);
                 appname.setText("ON");
-                databaseReference.child("User_01").child("nf").setValue(1);
-                editor.putInt("start", 1);//쉐어드 프리퍼런스
-                editor.apply();
+                //-----
+                startF(serviceIntent);
 
             }
         });
@@ -144,11 +164,10 @@ public class MainActivity extends AppCompatActivity {
         btncctv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(cctvip.isEmpty()){
-                    Toast.makeText(getApplicationContext(),"설정에서 CCTV ip를 입력하세요",Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://"+cctvip+"/stream"));
+                if (cctvip.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "설정에서 CCTV ip를 입력하세요", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + cctvip + "/stream"));
 
                     //Intent intent = new Intent(getApplicationContext(), CctvAcrivity.class);
                     // intent.putExtra("ip",cctvip);
@@ -171,11 +190,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
-                intent.putExtra("cctvip",cctvip)
-                        .putExtra("openonoff",openonoff)
-                        .putExtra("humanonoff",humanonoff)
-                        .putExtra("flameonoff",flameonoff)
-                        .putExtra("temp_humionoff",temp_humionoff);
+                intent.putExtra("cctvip", cctvip)
+                        .putExtra("openonoff", openonoff)
+                        .putExtra("humanonoff", humanonoff)
+                        .putExtra("flameonoff", flameonoff)
+                        .putExtra("temp_humionoff", temp_humionoff);
                 startActivity(intent);
             }
         });
@@ -198,83 +217,63 @@ public class MainActivity extends AppCompatActivity {
                         .addCategory(Intent.CATEGORY_LAUNCHER)
                         .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 //파이어베이스에서 각종 데이터를 읽어 온다
-                nf=user.getNf();
-                openS=user.getOpen();
-                humanS=user.getHuman();
-                cctvip=user.getCctv_ip();
-                openonoff=user.getOpen_onoff();
-                humanonoff=user.getHuman_onoff();
-                flameS=user.getFlame();
-                flameonoff=user.getFlame_onoff();
-                temp_humionoff=user.getTemp_humi_onoff();
+                openS = user.getOpen();
+                humanS = user.getHuman();
+                cctvip = user.getCctv_ip();
+                openonoff = user.getOpen_onoff();
+                humanonoff = user.getHuman_onoff();
+                flameS = user.getFlame();
+                flameonoff = user.getFlame_onoff();
+                temp_humionoff = user.getTemp_humi_onoff();
                 temp = user.getTemp();
                 humi = user.getHumi();
                 //쉐어드 프리퍼런스에 값을 가져오고 값이 1이면 서비스에 1값을 보내 포그라운드가 실행
-                start=pref.getInt("start",1);
-                if(start==1){
-                    serviceIntent.putExtra("start",1);
+                start = pref.getInt("start", 1);
+
+                if (start == 1) {
+                    serviceIntent.putExtra("start", 1);
                     editor.putInt("start", 0);
                     editor.apply();
+                } else if (start == 0) {
+                    serviceIntent.putExtra("start", 0);
                 }
-                else if(start==0){
-                    serviceIntent.putExtra("start",0);
-                }
 
 
+                //----------------------------------------------
 
 
-                if(nf==0){
+                openC = send_Service(openonoff, openS, opentext, openL, openC, serviceIntent, 1);//만들어 놓은 메소드를 각 센서에 맞게 적용
+
+
+                //----------------------------------------------
+
+                humanC = send_Service(humanonoff, humanS, humantext, humanL, humanC, serviceIntent, 2);
+                //----------------------------------------------
+
+
+                flameC = send_Service(flameonoff, flameS, flametext, flameL, flameC, serviceIntent, 3);
+                //----------------------------------------------
+                if (temp_humionoff == 1) {
+
+                    temp_humitext.setText(temp + "ºC / " + humi + "%");
+                } else
+                    temp_humitext.setText("센서 꺼짐");
+                //----------------------------------------------
+                Bsave = btnsave.getInt("Bsave", 1);
+
+                if (Bsave == 0) {
                     btnon.setVisibility(View.INVISIBLE);
                     btnoff.setVisibility(View.VISIBLE);
                     appname.setText("OFF");
-                }
-                else {
+                } else {
                     btnon.setVisibility(View.VISIBLE);
                     btnoff.setVisibility(View.INVISIBLE);
                     image = (ImageView) findViewById(R.id.connect);
                     Glide.with(getApplicationContext()).load(R.drawable.connect2).into(image);
                     appname.setText("ON");
+                    startF(serviceIntent);
                 }
 
-                if(nf==1){
-                    serviceIntent.putExtra("onoff",1);
-
-                }
-                else {
-                    serviceIntent.putExtra("onoff",0);
-
-                }
-                //----------------------------------------------
-
-
-                openC=send_Service(openonoff,openS,opentext,openL,openC,serviceIntent,1);//만들어 놓은 메소드를 각 센서에 맞게 적용
-
-
-                //----------------------------------------------
-
-                humanC=send_Service(humanonoff,humanS,humantext,humanL,humanC,serviceIntent,2);
-                //----------------------------------------------
-
-
-                flameC=send_Service(flameonoff,flameS,flametext,flameL,flameC,serviceIntent,3);
-                //----------------------------------------------
-                if(temp_humionoff==1) {
-
-                    temp_humitext.setText(temp+"ºC / "+humi+"%");
-                }
-                else
-                    temp_humitext.setText("센서 꺼짐");
-                //----------------------------------------------
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-
-                    startForegroundService(serviceIntent);
-
-                }
-                else {
-
-                    startService(serviceIntent);
-                }
 
             }
 
--- app/src/main/java/com/example/testproject/testService.java
+++ app/src/main/java/com/example/testproject/testService.java
@@ -23,25 +23,17 @@ import static android.app.PendingIntent.FLAG_CANCEL_CURRENT;
 public class testService extends Service {
 
 
+    public static testService testService;
+    String text = "센서 작동 중!!";
+    String text1 = "작동 중!";
+    long now;
+    int a = 2;
+    int opc, hmc, fmc, start, onoff, sensor;
+    SimpleDateFormat Format = new SimpleDateFormat("hh:mm:ss");
     private DatabaseReference databaseReferenceTime;
     private FirebaseDatabase database;
-    int sensor;
-    int onoff;
-    String text="센서 작동중!!";
-    String text1="센서 작동중!!";
-    long now;
     private Date date;
     private Intent testIntent;
-    int a =2;
-    int opc;
-    int hmc;
-    int fmc;
-    int start;
-    SimpleDateFormat Format = new SimpleDateFormat("hh:mm:ss");
-    public static testService testService;
-
-
-
 
     @Nullable
     @Override
@@ -55,7 +47,7 @@ public class testService extends Service {
 
         database = FirebaseDatabase.getInstance();
         databaseReferenceTime = database.getReference("timeline");
-        testService=this;
+        testService = this;
 
 
         // PendingIntent를 이용하면 포그라운드 서비스 상태에서 알림을 누르면 앱의 MainActivity를 다시 열게 된다.
@@ -64,29 +56,23 @@ public class testService extends Service {
                 .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .addCategory(Intent.CATEGORY_LAUNCHER);
 
-        start=intent.getIntExtra("start",0);
-        sensor=intent.getIntExtra("sensor",0);
-        onoff=intent.getIntExtra("onoff",100);
+        start = intent.getIntExtra("start", 0);
+        sensor = intent.getIntExtra("sensor", 0);
 
 
         PendingIntent pendingIntent
                 = PendingIntent
-                .getActivity(this, 0, testIntent, FLAG_CANCEL_CURRENT );
+                .getActivity(this, 0, testIntent, FLAG_CANCEL_CURRENT);
 
-        if(onoff==1)
-            text1="작동중";
-        else
-            text1="작동 멈춤";
 
-        if(sensor==1&&opc==1&&onoff==1)
+        if (sensor == 1 && opc == 1 /*&& onoff == 1*/)
             text = "문열림이 감지 되었습니다.";
 
-        else if(sensor==2&&hmc==1&&onoff==1)
+        else if (sensor == 2 && hmc == 1 /*&& onoff == 1*/)
             text = "인체가 감지 되었습니다";
 
-        else if(sensor==3&&fmc==1&&onoff==1)
+        else if (sensor == 3 && fmc == 1 /*&& onoff == 1*/)
             text = "화염이 감지 되었습니다";
-
 
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -118,45 +104,39 @@ public class testService extends Service {
                     .setContentIntent(pendingIntent)
                     .setContentText(text1);
 
-            if(sensor==1&&opc==1&&onoff==1) {
+            if (sensor == 1 && opc == 1 /*&& onoff == 1*/) {
                 databaseReferenceTime.push().setValue(getTime() + " 문열림이 감지 되었습니다.");
-                opc=0;
+                opc = 0;
                 mNotificationManager.notify(a, notification.build());
                 a++;
-                if(a==50){
-                    a=2;
+                if (a == 50) {
+                    a = 2;
                 }
-            }
-
-            else if(sensor==2&&hmc==1&&onoff==1) {
+            } else if (sensor == 2 && hmc == 1 /*&& onoff == 1*/) {
                 databaseReferenceTime.push().setValue(getTime() + " 인체가 감지 되었습니다.");
-                hmc=0;
+                hmc = 0;
                 mNotificationManager.notify(a, notification.build());
-                //
-                a++;
-                if(a==50){
-                    a=2;
-                }
-            }
 
-            else if(sensor==3&&fmc==1&&onoff==1) {
+                a++;
+                if (a == 50) {
+                    a = 2;
+                }
+            } else if (sensor == 3 && fmc == 1 /*&& onoff == 1*/) {
                 databaseReferenceTime.push().setValue(getTime() + " 화염이 감지 되었습니다.");
-                fmc=0;
+                fmc = 0;
                 mNotificationManager.notify(a, notification.build());
                 //
                 a++;
-                if(a==50){
-                    a=2;
+                if (a == 50) {
+                    a = 2;
                 }
-            }
-
-            else if(sensor==0) {
-                opc=1;
-                hmc=1;
-                fmc=1;
+            } else if (sensor == 0) {
+                opc = 1;
+                hmc = 1;
+                fmc = 1;
             }
             // foreground에서 시작
-            if(start==1){
+            if (start == 1) {
                 startForeground(1, notification1.build());
             }
 
@@ -164,7 +144,7 @@ public class testService extends Service {
         return START_STICKY;
     }
 
-    private String getTime(){
+    private String getTime() {
         now = System.currentTimeMillis();
         date = new Date(now);
         return Format.format(date);
